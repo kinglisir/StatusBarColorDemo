f4eaed82eaa6ea673ac7c36649a762cf7782c304
kinglisir <295487906@qq.com>
2016-07-05 17:15:13 +0800

    改变应用状态栏的颜色

---
+++ src/com/example/statusbarcolordemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.statusbarcolordemo;
+
+
+import android.support.v7.app.ActionBarActivity;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		activityConfig();
+	}
+
+	protected void activityConfig() {
+		// 设置手机屏幕为竖屏
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		// 设置状态栏透明（只有在4.4系统才有效果）
+		if (VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
+			setTranslucentStatus(true);
+			SystemBarTintManager tintManager = new SystemBarTintManager(this);
+			tintManager.setStatusBarTintEnabled(true);
+			tintManager.setStatusBarTintResource(R.color.nav_bg_color);
+		}
+	}
+
+	@TargetApi(19)
+	private void setTranslucentStatus(boolean on) {
+		Window win = getWindow();
+		WindowManager.LayoutParams winParams = win.getAttributes();
+		final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+		if (on) {
+			winParams.flags |= bits;
+		} else {
+			winParams.flags &= ~bits;
+		}
+		win.setAttributes(winParams);
+	}
+}
